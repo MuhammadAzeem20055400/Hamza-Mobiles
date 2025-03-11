524b69bab745aaf7281072c7ded9b298c2e22e5f
azeemqayyum <azeemqayyum055@gmail.com>
2025-03-11 07:15:25 +0500

    Project Setup With Database
    Set up Flask app with configuration for MySQL using SQLAlchemy.
    
    Created initial database schema with models for Admin, Customer, Product, and SalesTransaction.
    Added basic route and templates for index.

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from extensions import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/hamza_mobile'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
